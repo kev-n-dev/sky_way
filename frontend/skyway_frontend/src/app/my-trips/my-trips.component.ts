5f3e283f560a73ddeb28ce1d98b913b69d5c0231
kevin <kevin@paay.co>
2024-11-04 16:47:03 -0400

    added  basic my trips component

---
+++ frontend/skyway_frontend/src/app/my-trips/my-trips.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-trips',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-trips.component.html',
+  styleUrl: './my-trips.component.css'
+})
+export class MyTripsComponent {
+
+}
