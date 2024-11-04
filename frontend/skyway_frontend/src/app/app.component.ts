532505dc66d482861ac6616a160c3d61a71389c0
kevin <kevin@paay.co>
2024-11-04 16:48:27 -0400

    added routing

---
+++ frontend/skyway_frontend/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FlightResultsComponent } from './flight-results/flight-results.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet ,RouterModule, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'SkyWay';
+  items = [
+    { title: 'search', link: '/flight-search' }
+  ];
+}
---
+++ frontend/skyway_frontend/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { FlightSearchComponent } from './searchHome/flight-search/flight-search.component';
+import { PassengerInfoComponent } from './passenger-info/passenger-info.component';
+import { SummaryComponent } from './summary/summary.component';
+import { SuccessComponent } from './success/success.component';
+import { MyTripsComponent } from './my-trips/my-trips.component';
+import { HomePageComponent } from './searchHome/home-page/home-page.component';
+import { FlightResultsComponent } from './flight-results/flight-results.component';
+import { PassengerListComponent } from './passengers-list/passengers-list.component';
+
+export const routes: Routes  = [
+    { path: 'passenger-info', component: PassengerInfoComponent },
+    { path: 'summary', component: SummaryComponent },
+    { path: 'success', component: SuccessComponent },
+    { path: 'flight-search', component: FlightSearchComponent },
+    { path: 'my-trips', component: MyTripsComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: 'explore', component: FlightResultsComponent },
+    { path: 'booking/:flightId/:guests', component: PassengerListComponent },
+
+     { path: '', redirectTo: '/home', pathMatch: 'full' } // Default route
+  ];
+
+
+ 
+
+  
