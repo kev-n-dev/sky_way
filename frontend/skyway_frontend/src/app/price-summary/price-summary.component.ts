0bd3cba8fd65e3705e8f12707b6d39e87fce2043
kevin <kevin@paay.co>
2024-11-04 16:49:46 -0400

    added PLACEHOLDER COMPONENTS

---
+++ frontend/skyway_frontend/src/app/price-summary/price-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './price-summary.component.html',
+  styleUrl: './price-summary.component.css'
+})
+export class PriceSummaryComponent {
+
+}
