c63aa74acf796a48753ba0a024041bcf3c31473b
kevin <kevin@paay.co>
2024-11-05 00:02:45 -0400

    updated  with success and payment components

--- frontend/skyway_frontend/src/app/app.component.ts
+++ frontend/skyway_frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { FlightResultsComponent } from './flight-results/flight-results.component';
+import { FlightResultsComponent } from './booking/flight-results/flight-results.component';
 
 @Component({
   selector: 'app-root',
--- frontend/skyway_frontend/src/app/app.routes.ts
+++ frontend/skyway_frontend/src/app/app.routes.ts
@@ -1,21 +1,21 @@
 import { Routes } from '@angular/router';
 import { FlightSearchComponent } from './searchHome/flight-search/flight-search.component';
-import { PassengerInfoComponent } from './passenger-info/passenger-info.component';
-import { SummaryComponent } from './summary/summary.component';
-import { SuccessComponent } from './success/success.component';
+import { PassengerInfoComponent } from './booking/passenger-info/passenger-info.component';
+import { SuccessComponent } from './payment/success/success.component';
 import { MyTripsComponent } from './my-trips/my-trips.component';
 import { HomePageComponent } from './searchHome/home-page/home-page.component';
-import { FlightResultsComponent } from './flight-results/flight-results.component';
-import { PassengerListComponent } from './passengers-list/passengers-list.component';
+import { FlightResultsComponent } from './booking/flight-results/flight-results.component';
+import { PassengerListComponent } from './booking/passengers-list/passengers-list.component';
+import { SummaryAndPaymentComponent } from './payment/summary-and-payment/summary-and-payment.component';
 
 export const routes: Routes  = [
     { path: 'passenger-info', component: PassengerInfoComponent },
-    { path: 'payment/summary/:bookingId', component: SummaryComponent },
-    { path: 'success', component: SuccessComponent },
+    { path: 'payment/summary/:bookingId', component: SummaryAndPaymentComponent },
     { path: 'flight-search', component: FlightSearchComponent },
     { path: 'my-trips', component: MyTripsComponent },
     { path: 'home', component: HomePageComponent },
     { path: 'explore', component: FlightResultsComponent },
+    { path: 'booking/confirmation/:bookingId', component: SuccessComponent },
     { path: 'booking/:flightId/:guests', component: PassengerListComponent },
 
      { path: '', redirectTo: '/home', pathMatch: 'full' } // Default route
