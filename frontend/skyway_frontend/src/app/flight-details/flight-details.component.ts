9a02530e23c58f8774633d734165785ac8801d6a
kevin <kevin@paay.co>
2024-11-05 00:05:52 -0400

    added flight details component

---
+++ frontend/skyway_frontend/src/app/flight-details/flight-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input  } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './flight-details.component.html',
+  styleUrl: './flight-details.component.css'
+})
+export class FlightDetailsComponent {
+  @Input() from: string = ''; // Default value if not provided
+  @Input() to: string = ''; // Default value if not provided
+  @Input() date: string = ''; // Default value if not provided
+  @Input() passengers: number = 1; // Default value if not provided
+}
