6e7d1e63aecf50a9a86f9d43fdd0705c4ffaac5a
kevin <kevin@paay.co>
2024-11-06 01:15:24 -0400

    Update .gitignore to ignore all subfolders under /backend/static/

--- backend/static/skyway_frontend/server/main.server.mjs
+++ backend/static/skyway_frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RJKLJFBI.mjs";import"./chunk-EVEHMXCT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BNNHW32U.mjs";import"./chunk-W73R7N2D.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
